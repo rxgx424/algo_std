fb1eca41d90654188550a5a242c21277c45f0675
rxgx424 <130213698+rxgx424@users.noreply.github.com>
2025-04-17 12:14:20 +0900

    [level 2] Title: 구명보트, Time: 10.61 ms, Memory: 57.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        
+        Arrays.sort(people);
+        int min = 0;
+        int max = people.length - 1;
+        while(min<=max) {
+        	if(people[max]+people[min]<=limit) {
+        		min++;
+        	}
+        	max--;
+        	answer++;
+        	
+        }
+        
+        return answer;
+    }
+}
